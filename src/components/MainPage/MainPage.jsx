3881cdf63e1456dd5c666767ce15724a9d2f4c55
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-08 09:52:15 +0200

    modify placeholder

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -26,7 +26,7 @@ export const MainPage = () => {
     <div className="MainPage">
       <input type="text" 
       className="form-control" 
-      placeholder="Walter White"
+      placeholder="Write name of your Favorive 💡"
       onChange={(e) => filter(e.target.value)}/>
 
       <div className="row">
